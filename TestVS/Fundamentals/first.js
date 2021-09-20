15cbba19bc71fc6ed043f7105b87cfbe5245eb0f
Anonymous-DuckS <71487569+Anonymous-DuckS@users.noreply.github.com>
2021-09-20 15:47:57 +0800

    JS Fundamentals

---
+++ TestVS/Fundamentals/first.js
@@ -0,0 +1,20 @@
+var age = 44;
+var thisStatus = -10;
+
+var gpa = 3.45;
+
+//alert("Mark is " + age + " years old");
+
+output = "";
+
+output += "Age: " + age;
+output += "<br/>";
+output += "Status: " + thisStatus;
+output += "<br/>";
+output += "GPA: " + gpa;
+
+
+var fullName = "Sijay Pathanah";
+
+
+document.getElementById('out').innerHTML = fullName + "<br/>" +output;
